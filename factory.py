fcdb07665e688ca08b478adb4c04f5cd5031ee89
Henry <h.bekker@mail.ru>
2018-10-12 14:48:48 +0300

    rabbit +

--- factory.py
+++ factory.py
@@ -5,27 +5,30 @@ import pika
 
 routes = web.RouteTableDef()
 
+#Клиент запрашивает состояние
 @routes.get('/')
 async def get_handler(request):
 	#data = {'some': 'data'}
 	data = json_api.take('bd.json')
 	return web.json_response(data)
 
+#Клиент поставляет деталь
 @routes.post('/')
 async def post_handler(request):
 	if request.body_exists:
 		data = await request.text()
 	data = json.loads(data)
+	#отправляем деталь в склад
 	json_api.put(data)
 	print(json_api.take('bd.json'))
-
+	#отправляем сигнал worker-у
 	connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 	channel = connection.channel()
 	channel.queue_declare(queue='signal')
 	channel.basic_publish(exchange='',
                       routing_key='signal',
-                      body=data)
+                      body=json.dumps(data))
 	return web.Response( text="Принято")
 
 app = web.Application()
